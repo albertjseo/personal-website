8f6e90437e595084845e4ef2ef0cfdee884b53c4
albertjseo <albertjseo@gmail.com>
2024-11-03 16:22:48 -0800

    Updating projects.

--- app.py
+++ app.py
@@ -34,5 +34,35 @@ def contact():
     return render_template("base.html", content=Markup(page))
 
 
+@app.route('/covid_sql', methods=["GET"])
+def project1():
+    page = render_template("individual_projects/covid_sql.html")
+    return render_template("base.html", content=Markup(page))
+
+
+@app.route('/valorant_tracker', methods=["GET"])
+def project2():
+    page = render_template("individual_projects/valorant_tracker.html")
+    return render_template("base.html", content=Markup(page))
+
+
+@app.route('/agm_db', methods=["GET"])
+def project3():
+    page = render_template("individual_projects/agm_db.html")
+    return render_template("base.html", content=Markup(page))
+
+
+@app.route('/cell_morphology', methods=["GET"])
+def project4():
+    page = render_template("individual_projects/cell_morphology.html")
+    return render_template("base.html", content=Markup(page))
+
+
+@app.route('/drug_discovery', methods=["GET"])
+def project5():
+    page = render_template("individual_projects/drug_discovery.html")
+    return render_template("base.html", content=Markup(page))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
